9658da44b6a1865df2d21909ff9397985be8e1fe
imaftab01 <127646386+imaftab01@users.noreply.github.com>
2023-03-25 17:09:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,159 @@
+const currentTime = document.querySelector("#current-time");
+const setHours = document.querySelector("#hours");
+const setMinutes = document.querySelector("#minutes");
+const setSeconds = document.querySelector("#seconds");
+const setAmPm = document.querySelector("#am-pm");
+const setAlarmButton = document.querySelector("#submitButton");
+const alarmContainer = document.querySelector("#alarms-container");
+
+// Adding Hours, Minutes, Seconds in DropDown Menu for setting an Alarm
+window.addEventListener("DOMContentLoaded", (event) => {
+  
+  dropDownMenu(1, 12, setHours);
+ 
+  dropDownMenu(0, 59, setMinutes);
+
+  dropDownMenu(0, 59, setSeconds);
+
+  setInterval(getCurrentTime, 1000);
+  fetchAlarm();
+});
+
+// Event Listener added to Set Alarm Button
+setAlarmButton.addEventListener("click", getInput);
+
+// Adding zeroes to its drop down menu
+function dropDownMenu(start, end, element) {
+  for (let i = start; i <= end; i++) {
+    const dropDown = document.createElement("option");
+    dropDown.value = i < 10 ? "0" + i : i;
+    dropDown.innerHTML = i < 10 ? "0" + i : i;
+    element.appendChild(dropDown);
+  }
+}
+
+// getCurrent() time funtion is to fetch current time 
+function getCurrentTime() {
+  let time = new Date();
+  time = time.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  });
+  currentTime.innerHTML = time;
+
+  return time;
+}
+
+
+function getInput(e) {
+  e.preventDefault();
+  const hourValue = setHours.value;
+  const minuteValue = setMinutes.value;
+  const secondValue = setSeconds.value;
+  const amPmValue = setAmPm.value;
+
+  const alarmTime = convertToTime(
+    hourValue,
+    minuteValue,
+    secondValue,
+    amPmValue
+  );
+  setAlarm(alarmTime);
+}
+
+// Converting time to 24 hour format
+function convertToTime(hour, minute, second, amPm) {
+  return `${parseInt(hour)}:${minute}:${second} ${amPm}`;
+}
+
+// For notifying that Alarm time has reached and its ringing
+function setAlarm(time, fetching = false) {
+  const alarm = setInterval(() => {
+    if (time === getCurrentTime()) {
+      alert("Alarm Ringing");
+    }
+    console.log("running");
+  }, 500);
+
+  addAlaramToDom(time, alarm);
+  if (!fetching) {
+    saveAlarm(time);
+  }
+}
+
+// Displaying Alarms set by users in HTML
+function addAlaramToDom(time, intervalId) {
+  const alarm = document.createElement("div");
+  alarm.classList.add("alarm", "mb", "d-flex");
+  alarm.innerHTML = `
+              <div class="time">${time}</div>
+              <button class="btn delete-alarm" data-id=${intervalId}>Delete</button>
+              `;
+  const deleteButton = alarm.querySelector(".delete-alarm");
+  deleteButton.addEventListener("click", (e) => deleteAlarm(e, time, intervalId));
+
+  alarmContainer.prepend(alarm);
+}
+
+// Alarms saved in Local Storage?
+function checkAlarams() {
+  let alarms = [];
+  const isPresent = localStorage.getItem("alarms");
+  if (isPresent) alarms = JSON.parse(isPresent);
+
+  return alarms;
+}
+
+// Saving alarm to local storage
+function saveAlarm(time) {
+  const alarms = checkAlarams();
+
+  alarms.push(time);
+  localStorage.setItem("alarms", JSON.stringify(alarms));
+}
+
+// Getting alarms from local storage
+function fetchAlarm() {
+  const alarms = checkAlarams();
+
+  alarms.forEach((time) => {
+    setAlarm(time, true);
+  });
+}
+
+// Deleting exixting Alarm
+function deleteAlarm(event, time, intervalId) {
+  const self = event.target;
+
+  clearInterval(intervalId);
+
+  const alarm = self.parentElement;
+  console.log(time);
+
+  deleteAlarmFromLocal(time);
+  alarm.remove();
+}
+//Deleting alarm from local storage fuction 
+function deleteAlarmFromLocal(time) {
+  const alarms = checkAlarams();
+
+  const index = alarms.indexOf(time);
+  alarms.splice(index, 1);
+  localStorage.setItem("alarms", JSON.stringify(alarms));
+}
+
+// Clearing all the set alarms from local storage as well
+const clearAlarmsButton = document.querySelector("#clearAlarmsButton");
+clearAlarmsButton.addEventListener("click", clearAlarms);
+
+function clearAlarms() {
+  const alarms = document.querySelectorAll(".alarm");
+  alarms.forEach((alarm) => {
+    const intervalId = alarm.querySelector(".delete-alarm").getAttribute("data-id");
+    clearInterval(intervalId);
+    alarm.remove();
+  });
+  localStorage.removeItem("alarms");
+}
